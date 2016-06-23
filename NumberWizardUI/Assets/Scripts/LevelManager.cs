8d7f889e27fba27974a14c691bb57e8602d36272
Mobeen Aftab <mobeenaftab1@gmail.com>
2016-06-23 20:17:42 +0100

    Number Wizard UI
    this version runs on the UI instead of the unity console

---
+++ NumberWizardUI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+//Level manager is being used to control the UI inputs to change the game scene
+	//has to be public to be used in other unity IDE
+	public void LoadLevel(string name)
+	{
+		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("I want to quit");
+		Application.Quit();	//this method is bad practice and should not be used - it will be rejected if you try to publsish application. 
+	}
+
+	public void MainMenu (string name)
+	{
+		Debug.Log("Main Menu Request");
+		Application.LoadLevel(name);
+
+	}
+	/* note: if your plan is to pass the user through to a new scene in your game you need to define a string
+			variable parameter in the method and input what scene is passed through next on the 'onClick' event
+			in unity. other wise it will not show up in the drowp down function menu of said click event.
+	*/
+}
---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,76 @@
+﻿	//Mobeen Aftab
+	//29/12/2015
+
+	using UnityEngine;
+	using System.Collections;
+	using UnityEngine.UI;
+
+	public class NumberWizard : MonoBehaviour {
+
+	//global scope variables that are not initilised but defined so 
+	//both start and update functions can call variables
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessesAllowed = 10;	//this can be changed in script inspector
+	public Text text;
+
+	// Use this for initialization
+	void Start () 
+	{
+	 	StartGame();
+	}
+
+	//both start game and next guess are class methods that are being called into 
+	//the starts and update function. your code should look like this for best 
+	//practice - using methods and calling parameter functions
+
+	void StartGame() 
+	{
+		max = 1000;
+		min = 1;
+		NextGuess();
+	}
+
+
+
+	//Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKeyDown(KeyCode.UpArrow))
+	    {
+	        //print("up arrow presed");
+			GuessHigher();
+	    }
+	    else if (Input.GetKeyDown(KeyCode.DownArrow))
+	    {
+	        //print("down arrow presed");
+			GuessLower();
+	    }
+	} 
+
+	public void GuessHigher ()
+	{
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower ()
+	{
+		max = guess;
+		NextGuess ();
+	}
+
+	void NextGuess ()
+	{
+		guess = Random.Range(min,max+1);
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		text.text = guess.ToString();
+		if (maxGuessesAllowed <= 0) 
+		{	//if 10incorect guesses have been made the player wins
+			Application.LoadLevel("Win");
+		}
+	}
+
+	}
