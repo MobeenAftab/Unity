39ddb5f6f652053b3a1cedd63423023bb536d365
Mobeen Aftab <mobeenaftab1@gmail.com>
2016-10-03 18:21:29 +0100

    Glitch Gardern
    Added new features, defenders spawn on grid selection and follow
    inspector clone tab best practice

---
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	public Camera myCamera;
+
+	private GameObject defenderParent;
+
+	void Start() {
+		//sub all defender spws under one defender element in the inspector - like the projectile object
+		defenderParent = GameObject.Find ("Defenders");
+		if(!defenderParent) {
+			defenderParent = new GameObject("Defenders");
+		}
+	}
+
+	void OnMouseDown() {
+		//print mouse position on click in console
+		//print (Input.mousePosition);
+		//print ( SnapToGrid(CalculateWorldPointOfMouseClick()) );
+
+		//spawn defender at location of grid after button select
+		Vector2 rawPos = CalculateWorldPointOfMouseClick();	//get raw position
+		Vector2 roundedPos = SnapToGrid (rawPos);	//use method to round
+		GameObject defender = Button.selectedDefender;	//get selected defender
+		Quaternion zeroRotation = Quaternion.identity; //unity required var in instantiate
+		GameObject newDef = Instantiate (defender, roundedPos, zeroRotation) as GameObject;	
+		//instanciates new defenders and spawns them at rounded position under defender element
+		newDef.transform.parent = defenderParent.transform;	//assigns defenders a parent def on spawn
+	}
+
+	//get unrounded world space pos and round to grid center
+	Vector2 SnapToGrid (Vector2 rawWorldPos) {
+		float newX = Mathf.RoundToInt (rawWorldPos.x);
+		float newY = Mathf.RoundToInt (rawWorldPos.y);
+		return new Vector2 (newX, newY);
+	}
+
+	Vector2 CalculateWorldPointOfMouseClick() {
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		float distanceFromCamera = 10f;	//no Z depth in the camera perspective
+
+		//weirdTriplet because Z position is invalid in our camera scope this distfromCam is set to 10f
+		Vector2 weirdTriplet = new Vector3 (mouseX, mouseY, distanceFromCamera);
+		Vector2 worldPos = myCamera.ScreenToWorldPoint (weirdTriplet);
+
+		return worldPos;
+	}
+
+
+}
