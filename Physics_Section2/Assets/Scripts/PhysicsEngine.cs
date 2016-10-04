bee95ffb61071d2aba2a98cf2b8eb5dce5008f6e
Mobeen Aftab <mobeenaftab1@gmail.com>
2016-10-04 21:45:42 +0100

    Physics Section_2
    Added newtons second law of motion and support for github version
    control

--- Physics_Section2/Assets/Scripts/PhysicsEngine.cs
+++ Physics_Section2/Assets/Scripts/PhysicsEngine.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class PhysicsEngine : MonoBehaviour {
 
+	public float mass;
 	public Vector3 velocityVector;	//average velocity for fixedUpdate()
 	public Vector3 netForceVector;
 	public List<Vector3> forceVectorList = new List<Vector3> ();	//add multiple forces to object
@@ -15,12 +16,20 @@ public class PhysicsEngine : MonoBehaviour {
 	//called at a constant 20ms
 	void FixedUpdate() {
 		AddForces ();
+		UpdateVelocity ();
+
+		//update position
+		transform.position += velocityVector * Time.deltaTime;
+
+
+		/* no longer detecting a change of net velocity
 		if (netForceVector == Vector3.zero) {	//if no net force
 			//adding to the current position the ammount of displacment velocityVector
 			transform.position += velocityVector * Time.deltaTime;
 		} else { //if net force unbalanced
 			Debug.LogError("Unbalanced for detected, help !");
 		}
+		*/
 	}
 
 	void AddForces () {
@@ -29,5 +38,12 @@ public class PhysicsEngine : MonoBehaviour {
 		foreach (Vector3 forceVector in forceVectorList) {
 			netForceVector = netForceVector + forceVector;
 		}	//add all the forces and outputs results
+	}
+
+	//Newtons second law of motion
+	void UpdateVelocity () {
+		Vector3 accelerationVector = netForceVector / mass;	// F = ma
+		//change of velocity depends on duration of force
+		velocityVector += accelerationVector * Time.deltaTime;
 	}
 }
