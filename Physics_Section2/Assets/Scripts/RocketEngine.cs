b8284057229d7d7677a7288e8ffd982743fb73a8
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-03-11 15:09:14 +0000

    Update [hysics engine
    added forces for calculating fuel consumtion and velocity force

---
+++ Physics_Section2/Assets/Scripts/RocketEngine.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class RocketEngine : MonoBehaviour {
+
+	public float fuelMass;		// [Kg]
+	public float maxThrust;		// kN [kg m s^-2]
+
+	[Range(0, 1f)]
+	public float thrustPercent;	// [none]
+	public Vector3 thrustUnitVector;	//[none}
+
+	private PhysicsEngine physicsEngine;
+	private float currentThrust;	// N
+
+	// Use this for initialization
+	void Start () {
+		physicsEngine = GetComponent<PhysicsEngine>();
+		physicsEngine.mass += fuelMass;
+	}
+
+	void FixedUpdate () {
+		if (fuelMass > fuelUpdate ()) {
+			fuelMass -= fuelUpdate ();
+			physicsEngine.mass -= fuelUpdate ();
+			exertForce ();
+		} else {
+			Debug.LogWarning ("Fuel depleted");
+		}
+	}
+
+	float fuelUpdate() {
+		float exhaustMassFlow;
+		float effectiveExhaustVelocity = 4463f;
+
+		exhaustMassFlow = currentThrust / effectiveExhaustVelocity;
+
+		return exhaustMassFlow * Time.deltaTime;
+	}
+
+	void exertForce() {
+		currentThrust = thrustPercent * maxThrust * 1000f;
+		Vector3 thrustVector = thrustUnitVector.normalized * currentThrust;
+		physicsEngine.AddForce (thrustVector);
+	}
+
+}
