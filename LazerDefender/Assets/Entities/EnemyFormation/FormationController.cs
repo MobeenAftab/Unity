092d60cae1312b2b46cc54d74c8c3b31ac7f006f
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-03-19 00:40:31 +0000

    Lazer Defender
    Uploading lazer defender game

---
+++ LazerDefender/Assets/Entities/EnemyFormation/FormationController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FormationController : MonoBehaviour {
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 5f;
+	public float spawnDelay = 0.5f;
+	public float padding = 1;
+	
+	private bool movingRight = true;
+	private float xmax;
+	private float xmin;
+	
+	// Use this for initialization
+	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0,0, distanceToCamera));
+		Vector3 rightEdge = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distanceToCamera));
+		xmax = rightEdge.x;
+		xmin = leftBoundary.x;
+		SpawnUntilFull();
+	}
+	
+	void SpawnUntilFull(){
+		Transform freePosition = NextFreePosition();
+		if(freePosition){
+			GameObject enemy = Instantiate(enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = freePosition;
+		}
+		if(NextFreePosition()){
+			Invoke ("SpawnUntilFull", spawnDelay);
+		}
+	}
+	
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(movingRight){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}else{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+    	// Check if the formation is going outside the playspace...
+		float rightEdgeOfFormation = transform.position.x + (0.5f*width);
+		float leftEdgeOfFormation = transform.position.x - (0.5f*width);
+		if(leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax){
+			movingRight = !movingRight;
+		}
+		
+		if(AllMembersDead()){
+			Debug.Log("Empty Formation");
+			SpawnUntilFull();
+		}
+	}
+	
+	Transform NextFreePosition(){
+		foreach(Transform childPositionGameObject in transform){
+			if (childPositionGameObject.childCount == 0){
+				return childPositionGameObject;
+			}
+		}
+		return null;
+	}
+	
+	
+	bool AllMembersDead(){
+		foreach(Transform childPositionGameObject in transform){
+			if (childPositionGameObject.childCount > 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
---
+++ LazerDefender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 15.0f;
+	public GameObject projectile;
+	public AudioClip fireSound;
+	public float padding = 1f;
+	public float projectileSpeed = 10;
+	public float firingRate = 0.2f;
+	public float health = 250f;
+	
+	float xmin;
+	float xmax;
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log ("Player Collided with missile");
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Die();
+			}
+		}
+	}
+	
+	void Die(){
+		LevelManager man = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+		man.LoadLevel("Win Screen");
+		Destroy(gameObject);
+	}
+	
+	void Start(){
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+	
+	void Fire(){
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			InvokeRepeating("Fire", 0.0001f, firingRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke("Fire");
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime; 
+		}
+		
+		// restrict the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+	
+	
+	
+}
