519dcb046e37e5f09eb35cdad7d59ee6f591332b
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-06-27 01:00:21 +0100

    Block Dash
    Completed, added menu and credits

---
+++ Source/Assets/Scripts/Credits.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+
+	public void Begin() {
+		Debug.Log ("Begin");
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void PlayAgain() {
+		Debug.Log ("Restart");
+		SceneManager.LoadScene ("BlockRun_1");
+	}
+
+	public void Quit() {
+
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+}
