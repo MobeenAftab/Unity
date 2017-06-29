439cada76fa0a3d4130852a2d1a15a701b180bc7
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-06-29 08:26:18 +0100

    ProCam
    added new camera script

---
+++ Source/Assets/Scripts/ProCam.cs
@@ -0,0 +1,22 @@
+﻿/*
+ *	ProCam best used for a 2d platformer style of game
+ *
+*/
+using UnityEngine;
+
+public class ProCam : MonoBehaviour {
+
+	public Transform pos;
+
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+	void FixedUpdate() {
+		
+		Vector3 desiredPos = pos.position + offset;
+		Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed*Time.deltaTime);
+		transform.position = smoothedPos;
+
+		transform.LookAt (pos);
+	}
+}
