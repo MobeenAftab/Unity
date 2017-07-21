e802fd21c5e4f1eae50c4703d5c6c7f883c60d63
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-07-21 19:24:10 +0100

    DragonRPG
    Init commit of DragonRPG tut

---
+++ DragonRPG/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		//print (player.ToString ());
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position;
+		
+	}
+}
