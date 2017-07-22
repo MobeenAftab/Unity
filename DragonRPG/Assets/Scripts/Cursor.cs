62829b4ff9828de4dc24da935d02c12b72165daf
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-07-22 21:06:12 +0100

    DragonRPG
    point and click movement

---
+++ DragonRPG/Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	CameraRaycaster CR;
+
+	// Use this for initialization
+	void Start () {
+
+		CR = GetComponent<CameraRaycaster> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//print (CR.layerHit);
+	}
+}
