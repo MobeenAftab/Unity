743f4c3cf04e7f40c47bc4b872e89e3a6d02c31a
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-07-24 18:10:55 +0100

    DragonRPG
    Added coursor images

--- DragonRPG/Assets/Scripts/CursorAffordance.cs
+++ DragonRPG/Assets/Scripts/CursorAffordance.cs
@@ -3,18 +3,37 @@
 // An affordance is the possibillity of an action on an object or enviroment
 public class CursorAffordance : MonoBehaviour {
 
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D attackCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+	[SerializeField] Vector2 hotSpot = new Vector2(96, 96);
+
 	CameraRaycaster CR;
 
 	// Use this for initialization
 	void Start () {
 
-		CR = GetComponent<CameraRaycaster> ();
-		
+		CR = GetComponent<CameraRaycaster> ();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		switch (CR.layerHit) {
+
+			case Layer.Walkable:
+				Cursor.SetCursor (walkCursor, hotSpot, CursorMode.Auto);
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor (unknownCursor, hotSpot, CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor (attackCursor, hotSpot, CursorMode.Auto);
+				break;
+			default:
+				Debug.Log ("Unknown Layer tag");
+				return;
+		}
 		//print (CR.layerHit);
 	}
 }
