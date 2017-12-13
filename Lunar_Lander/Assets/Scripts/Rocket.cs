890ca449a964a37774e2b24926066f95d4e13ccd
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-12-13 22:50:05 +0000

    Lunar_Lander
    Basic game framework setup

---
+++ Lunar_Lander/Assets/Scripts/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		// Get rigidBody of rocket ship
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		processInput();
+	}
+
+	private void processInput() {
+		if (Input.GetKey(KeyCode.Space)) {
+			rigidBody.AddRelativeForce(Vector3.up);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			print ("Rotate Left");
+		} else if (Input.GetKey(KeyCode.D)) {
+			print ("Rotate Right");
+		}
+
+	}
+
+}
