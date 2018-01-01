2e92900bbf947d5f6dd6c75cbcbaca129f7cc829
Mobeen Aftab <mobeenaftab1@gmail.com>
2018-01-01 20:30:25 +0000

    Lunar_Lander
    Added second level

--- Lunar_Lander/Assets/Scripts/Rocket.cs
+++ Lunar_Lander/Assets/Scripts/Rocket.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
+    // TODO:: Add sound for rocket
+
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    float transitionDelay = 1f;
 
 	Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -19,8 +26,10 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        thrust();
-        rotate();
+        if (state == State.Alive) {
+            thrust();
+            rotate();   
+        }
 	}
 
     private void rotate() {
@@ -55,17 +64,34 @@ public class Rocket : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision collision) {
+
+        // Exit method gaurd condition
+        if (state != State.Alive) {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("OK");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("loadNextLevel", transitionDelay);
                 break;
             default:
-                print("Dead");
+                print("You Died");
+                state = State.Dying;
+                Invoke("loadFirstLevel", transitionDelay);
                 break;
         }
 	}
+
+    private void loadNextLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+    private void loadFirstLevel() {
+        SceneManager.LoadScene(0);
+    }
 }
