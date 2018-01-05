c8bedb26c2d9801893e78a19fab678da054aea74
Mobeen Aftab <mobeenaftab1@gmail.com>
2018-01-05 23:47:09 +0000

    Lunar_Lander
    Oscillator scrip move game object dynamically

---
+++ Lunar_Lander/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿// Script to move objects in game scene dynamically
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attribute to stop duplicate script on component
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
