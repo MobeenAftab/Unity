54ac096452d561f6fef9fa7d230a6755bb129ff0
mobeenaftab <mobeenaftab1@gmail.com>
2018-06-20 22:03:55 +0100

    UGDE_02
    Added Shooter script for main camera

---
+++ UGDE/UGDE_02/Assets/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    // Public member vairables
+    public Rigidbody bullet;
+    public float power = 1500f;
+    public float moveSpeed = 2f;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
