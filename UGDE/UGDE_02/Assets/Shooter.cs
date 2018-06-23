50feb46fe9619279e95900e55e167e075c98b137
mobeenaftab <mobeenaftab1@gmail.com>
2018-06-23 20:01:14 +0100

    UGDE
    Added projectile shooter, Finished prototype

--- UGDE/UGDE_02/Assets/Shooter.cs
+++ UGDE/UGDE_02/Assets/Shooter.cs
@@ -11,6 +11,21 @@ public class Shooter : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+		// Get the axis from the Input manager 
+		float horizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed;
+		float vertical = Input.GetAxis ("Vertical") * Time.deltaTime * moveSpeed;
+
+		// Move camera on axis using W,A,S,D
+		transform.Translate (horizontal, vertical, 0);
+
+		// If left mouse click pressed 
+		if (Input.GetButtonUp ("Fire1")) {
+			// Instanciate Projectile on click
+			Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+			// Add forward (Z-axis) force on instanciated object
+			Vector3 forwardforce = transform.TransformDirection (Vector3.forward);
+			// Move objetc in Z-axis in world space by this force amount
+			instance.AddForce(forwardforce * power);
+		}
 	}
 }
